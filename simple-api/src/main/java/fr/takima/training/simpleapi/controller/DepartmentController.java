c9d77cbe91b0ac510f4c4a4a3a0f9acbee13fce7
Taha RACHID <126984637+TRach07@users.noreply.github.com>
2025-05-23 11:33:19 +0200

    Update DepartmentController.java
    test

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+//protection test by taha
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/departments")
